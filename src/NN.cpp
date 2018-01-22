ce27a9e2470b29dcfb8d7d699a62e86a5df3fcb0
XOPHELIOUS <johnefillips@gmail.com>
2018-01-21 20:23:27 -0500

    Started outlining the computation

--- src/NN.cpp
+++ src/NN.cpp
@@ -14,6 +14,23 @@ double NeuralNetwork::sigmoid_prime(double x) {
     return t / ((1 + t) * (1 + t));
 }
 
-unsigned int NeuralNetwork::compute(Example e) {
-    // TODO
+Matrix<unsigned char> NeuralNetwork::feedForward(Matrix<double> input, Matrix<double> weights, Matrix<double> bias){
+	Matrix<double> result = weights * input;
+	for (int i = 0; i < result.rows(); i++) {
+		result[i][0] = sigmoid(result[i][0] - bias[i]);
+	}
+	return result;
+}
+
+unsigned int NeuralNetwork::compute(Matrix<unsigned char> firstLayer) {
+	Matrix<double> hiddenLayer(HIDDEN_SIZE, 1), lastLayer(OUTPUT_SIZE, 1);
+	hiddenLayer = feedForward(firstLayer, firstWeights, firstBias);
+	lastLayer = feedForward(hiddenLayer, secondWeights, secondBias);
+
+	int maxValIndex = 0;
+	for (int i = 1; i < 10; i++) {
+		if (lastLayer[i][0] > lastLayer[maxValIndex][0])
+			maxValIndex = i;
+	}
+	return maxValIndex; 
 }
